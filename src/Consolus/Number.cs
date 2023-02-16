744f384b44445a9d25a23f1a9ea4f90df2754b68
AmineelMeck <amine.e.m@hotmail.se>
2023-02-16 13:17:17 +0100

    Bs code commit

---
+++ src/Consolus/Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolus
+{
+    internal class Number
+    {
+        public Number()
+        {
+            int i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}
